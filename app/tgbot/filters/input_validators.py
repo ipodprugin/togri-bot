79c61b672d1595d461d7aa9b9be13475a2a27ffe
ipodprugin <p0dprugin.ilia@gmail.com>
2023-12-04 16:45:11 +0300

    corrected input validation filter

--- app/tgbot/filters/input_validators.py
+++ app/tgbot/filters/input_validators.py
@@ -10,7 +10,7 @@ class ValidTenderIdInput(BaseFilter):
         if len(lines) > 5:
             await message.reply('Я пока не могу обработать за раз такой объём информации. Введите не больше 5 значений')
             return False
-        for line in lines:
-            if not config.TENDER_ID_REGEX.findall(line):
-                return False
+        # for line in lines:
+        #     if not config.TENDER_ID_REGEX.findall(line):
+        #         return False
         return True
